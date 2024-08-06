be0f6d892d88f7f156d4d8b5fee889e275ca2c49
in7hesky <anton.maiko.qa@gmail.com>
2024-08-06 14:40:32 +0300

    add favicon

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from dynaconf import FlaskDynaconf
+import os
+from flask import send_from_directory
 
 def create_app():
     app = Flask(__name__)
@@ -13,6 +15,14 @@ def create_app():
         from . import info
         app.register_blueprint(info.info_bp)
         
+        @app.route('/favicon.ico')
+        def favicon():
+            return send_from_directory(
+                os.path.join(app.root_path, 'static/images'),
+                'favicon.ico',
+                mimetype='image/vnd.microsoft.icon',
+            )
+            
         return app
     
 
