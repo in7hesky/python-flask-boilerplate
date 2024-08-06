90a5320fbc2f8e5b4460e87a8afe4f1417e83eb3
in7hesky <anton.maiko.qa@gmail.com>
2024-08-06 14:23:48 +0300

    set up debug toolbar for dev env

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,12 @@ from dynaconf import FlaskDynaconf
 
 def create_app():
     app = Flask(__name__)
+
     
     with app.app_context():
-        FlaskDynaconf().init_app(app)
+        FlaskDynaconf(extensions_list=True).init_app(app)
+        
+        app.config.SECRET_KEY = bytearray(app.config.SECRET_KEY, "UTF-8")
         
         from . import info
         app.register_blueprint(info.info_bp)
