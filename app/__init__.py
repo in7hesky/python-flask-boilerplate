c5e16cd932d4418d4a9f9946331b467394ba796c
in7hesky <anton.maiko.qa@gmail.com>
2024-08-06 13:24:38 +0300

    set up dynaconf

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from dynaconf import FlaskDynaconf
 
 def create_app():
     app = Flask(__name__)
     
     with app.app_context():
+        FlaskDynaconf().init_app(app)
+        
         from . import info
         app.register_blueprint(info.info_bp)
         
